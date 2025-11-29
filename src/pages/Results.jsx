5ddb3fb8024ffaaf552afe0c0ab05811f0c130b6
bijodev <bijodev1@gmail.com>
2025-11-29 15:29:11 +0530

    add updated

--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { getQuizResultsFromFirestore, calculateWinners } from '../firebase/firebaseQuiz';
+import { getQuizResultsFromFirestore, calculateWinners } from '../utils/firebaseQuiz';
 
 const Results = () => {
   const [results, setResults] = useState(null);
